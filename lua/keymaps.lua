633e8a649da739f45dfae52bf03afb1fe42d3556
junjun5 <junhattori@google.com>
2025-07-11 16:48:35 +0900

    first

---
+++ lua/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("i","jj","<Esc>")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set('n', '<Leader>ee', ':NvimTreeToggle<CR>', { noremap = true, silent = true, desc = 'Toggle NvimTree' })
