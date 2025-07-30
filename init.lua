ad1c667c95c24126849fcc22ade1958026aa4a92
junjun5 <junhattori@google.com>
2025-07-30 21:28:48 +0900

    nvim-tree option and leader registed

--- init.lua
+++ init.lua
@@ -1,8 +1,22 @@
 require("options")
 require("keymaps")
 require("plugins")
+vim.g.mapleader = " "
 vim.cmd[[colorscheme tokyonight-night]]
 
+-- quitting the vim windows closing nvim-tree at the same time
+vim.keymap.set("n", "q", function()
+  -- 現在のウィンドウのタイプが 'NvimTree' の場合
+  if vim.api.nvim_win_get_option(0, "filetype") == "NvimTree" then
+    vim.cmd("NvimTreeClose") -- NvimTreeを閉じるコマンド
+    -- NvimTreeを閉じたら、もし他のバッファがない場合はNeovim自体も終了
+    if #vim.api.nvim_list_bufs() == 1 then -- NvimTreeを閉じた後に残るバッファが1つ以下の場合（通常はNvimTreeのバッファが0になると想定）
+        vim.cmd("qa") -- 全てのバッファを強制終了
+    end
+  else
+    vim.cmd("q") -- それ以外の場合は通常の :q を実行
+  end
+end, { desc = "現在のウィンドウを閉じる (NvimTree対応)" })
 
 -- For nvim-tree
 -- disable netrw at the very start of your init.lua
